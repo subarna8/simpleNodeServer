c5c285c57e708861485d9c81ed783f411e9580e6
Mahmuda Akhter <1510930@iub.edu.bd>
2018-07-16 04:55:20 +0000

    classwork6_1

--- nodeServer.js
+++ nodeServer.js
@@ -11,6 +11,9 @@ var http = require('http');
   app.get('/about', function(req, res){
     res.sendFile(__dirname+'/about.html');
   });
+   app.get('/index1', function(req, res){
+    res.sendFile(__dirname+'/index1.html');
+  });
 
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
