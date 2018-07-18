a72873964cf3184567decaa13bfff47e7fd35a8e
Mahmuda Akhter <1510930@iub.edu.bd>
2018-07-18 04:39:01 +0000

    classwork6_1

--- nodeServer.js
+++ nodeServer.js
@@ -1,19 +1,34 @@
 var http = require('http');
   var express = require('express');
   var app = express();
+  var bodyParser = require('body-parser');
   var server = http.Server(app);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}));
 
+  
 
   app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
   })
-
+  
   app.get('/about', function(req, res){
     res.sendFile(__dirname+'/about.html');
   });
+  
    app.get('/index1', function(req, res){
     res.sendFile(__dirname+'/index1.html');
   });
+  
+  app.post('/signup', function(req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    console.log("post received: %s %s", username, email);
+});
+app.post('/submit_user', function(req, res){
+  console.log(req.body);
+})
+
 
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
