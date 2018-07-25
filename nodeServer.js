89fb576a96a5953fb93bd6310d33b1cf1a6a5cf7
Mahmuda Akhter <1510930@iub.edu.bd>
2018-07-25 04:51:12 +0000

    classwork8

--- nodeServer.js
+++ nodeServer.js
@@ -3,6 +3,25 @@ var http = require('http');
   var app = express();
   var bodyParser = require('body-parser');
   var server = http.Server(app);
+  var mongo = require('mongodb');
+  //for local
+  //var db, uri = "mongodb://localhost:"
+  var db, uri = "mongodb://"+process.env.IP+":27017";
+  mongo.MongoClient.connect(uri,{useNewUrlParser:true}, 
+                                  function(err,client){
+                                    if(err){
+                                      console.log('Could not connect to MongoDB');
+                                    }
+                                    else{
+                                      db = client.db('node-cw8');
+                                    }
+                                  });
+  
+  var save = function(index1_data){
+    db.createCollection('users', function(err,collection){});
+    var collection = db.collection('users');
+    collection.save(index1_data);
+  };
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
 
@@ -10,7 +29,7 @@ var http = require('http');
 
   app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
-  })
+  });
   
   app.get('/about', function(req, res){
     res.sendFile(__dirname+'/about.html');
@@ -27,7 +46,9 @@ var http = require('http');
 });
 app.post('/submit_user', function(req, res){
   console.log(req.body);
-})
+  save(req.body);
+  res.status(200);
+});
 
 
   server.listen(process.env.PORT, process.env.IP, function(){
