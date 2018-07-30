4ef742b67ec0af46547859933d45f847d61a1a2c
Mahmuda Akhter <1510930@iub.edu.bd>
2018-07-30 04:46:43 +0000

    classwork9

--- nodeServer.js
+++ nodeServer.js
@@ -3,11 +3,34 @@ var http = require('http');
   var app = express();
   var bodyParser = require('body-parser');
   var server = http.Server(app);
-  var mongo = require('mongodb');
+  var mongoose = require('mongoose');
+  mongoose.connect("mongodb://localhost:27017/node-cw8");
+  
+  mongoose.connection.on('error', function(){
+    console.log("Could not connect to mongodb");
+  });
   //for local
   //var db, uri = "mongodb://localhost:"
-  var db, uri = "mongodb://"+process.env.IP+":27017";
-  mongo.MongoClient.connect(uri,{useNewUrlParser:true}, 
+ // var db, uri = "mongodb://"+process.env.IP+":27017";
+  
+  //var mongoose = require('mongoose');
+  //mongoose.connect(uri);
+  
+  //var Schema = mongoose.Schema;
+  
+  var userSchema = new mongoose.Schema({
+      name:{ 
+        type: String,
+        required: "Name is required"
+        
+      },
+      email: String
+    
+  });
+  
+  var User = mongoose.model('User', userSchema);
+  
+  /*mongo.MongoClient.connect(uri,{useNewUrlParser:true}, 
                                   function(err,client){
                                     if(err){
                                       console.log('Could not connect to MongoDB');
@@ -15,7 +38,7 @@ var http = require('http');
                                     else{
                                       db = client.db('node-cw8');
                                     }
-                                  });
+                                  });*/
   
   var save = function(index1_data){
     db.createCollection('users', function(err,collection){});
@@ -46,8 +69,13 @@ var http = require('http');
 });
 app.post('/submit_user', function(req, res){
   console.log(req.body);
-  save(req.body);
-  res.status(200);
+  var new_user = new User(req.body);
+  new_user.save(function(err, data){
+    if(err)
+    return res.status(400)
+              .json({message: "Could Not Save User"});
+              res.status(200).json(data);
+  })
 });
 
 
